acde1236cef6f38fa36231910cdf8df89bac0169
muslimfact <muslimfact900@gmail.com>
2025-03-11 21:11:47 +0700

    Update poop_jalan.py

--- poop_jalan.py
+++ poop_jalan.py
@@ -1,12 +1,20 @@
 import subprocess
 
 # Menjalankan poop_baru.py
-print("Menjalankan poop_baru.py...")
-proses1 = subprocess.run(["python3", "poop_baru.py"], check=True)
+print("Menjalankan poop_baru_old.py...")
+proses1 = subprocess.run(["python3", "poop_baru_old.py"], check=True)
 
 # Menjalankan remote_upload.py setelah poop_baru.py selesai
 print("Menjalankan remote_upload.py...")
 proses2 = subprocess.run(["python3", "remote_upload.py"], check=True)
+
+# Menjalankan poop_baru.py
+print("Menjalankan poop_baru.py...")
+proses3 = subprocess.run(["python3", "poop_baru.py"], check=True)
+
+# Menjalankan remote_upload.py setelah poop_baru.py selesai
+print("Menjalankan remote_upload.py...")
+proses4 = subprocess.run(["python3", "remote_upload.py"], check=True)
 
 # Print jika kedua script sudah dijalankan
 print("Semua script telah dijalankan!")
