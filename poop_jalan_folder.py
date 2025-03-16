ff3ddc21ad12eae00d6e23ad5036d8ccfbd2c5d4
muslimfact <muslimfact900@gmail.com>
2025-03-16 20:31:29 +0700

    Update poop_jalan_folder.py

--- poop_jalan_folder.py
+++ poop_jalan_folder.py
@@ -8,12 +8,7 @@ print("Menjalankan poop_folder_extract.py...")
 proses1 = subprocess.run(["python3", "poop_folder_extract.py"], check=True)
 
 
-print("Menjalankan poop_baru_old.py...")
-proses2 = subprocess.run(["python3", "poop_baru_old.py"], check=True)
 
-# Menjalankan remote_upload.py setelah poop_baru.py selesai
-print("Menjalankan remote_upload.py...")
-proses3 = subprocess.run(["python3", "remote_upload.py"], check=True)
 
 print("Menjalankan poop_baru.py...")
 proses5 = subprocess.run(["python3", "poop_baru.py"], check=True)
