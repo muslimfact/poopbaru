6345bfad07d95b5a27f0e2f885b548df295442e9
muslimfact <muslimfact900@gmail.com>
2025-04-18 15:35:39 +0700

    Update poop_jalan.py

--- poop_jalan.py
+++ poop_jalan.py
@@ -3,7 +3,7 @@ import subprocess
 
 # Menjalankan poop_baru.py
 print("Menjalankan poop_baru.py...")
-proses3 = subprocess.run(["python3", "poop_baru.py"], check=True)
+proses3 = subprocess.run(["python3", "poop2_baru.py"], check=True)
 
 # Menjalankan remote_upload.py setelah poop_baru.py selesai
 print("Menjalankan remote_upload.py...")
